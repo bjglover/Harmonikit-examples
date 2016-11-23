241dc7bc45af0a7482d137fab8913d8a81a5b8e7
Ben Glover <white6rabbit@yahoo.co.uk>
2016-11-23 13:19:16 +0000

    Updated :dependencies
    Updated for newer versions of clojure, overtone and core.async

--- project.clj
+++ project.clj
@@ -46,7 +46,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [overtone "0.8.1" :exclusions [org.clojure/clojure]]
-                 [org.clojure/core.async "0.1.256.0-1bf8cf-alpha"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [overtone "0.10.1" :exclusions [org.clojure/clojure]]
+                 [org.clojure/core.async "0.2.395"]]
   :jvm-opts ^:replace ~(jvm-opts))
